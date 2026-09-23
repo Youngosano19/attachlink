32e04bec68796817c2705736da52c79d5c46a4f4
Peter Young Osano <youngkiagai@gmail.com>
2026-09-23 22:07:10 +0300

    Move secrets to environment variables

--- core/settings.py
+++ core/settings.py
@@ -1,10 +1,13 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-change-this-in-production'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
